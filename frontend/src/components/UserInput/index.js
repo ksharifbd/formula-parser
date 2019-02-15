68962fca230ec33715c83aef7f3ebbb65d6b67ff
ksharifbd <ksharifbd@gmail.com>
2019-02-16 00:11:51 +0600

    locks font size of inout in UserInput

--- frontend/src/components/UserInput/index.js
+++ frontend/src/components/UserInput/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import MediaText from '../MediaText';
+import classes from './UserInput.module.css';
 
 const UserInput = ({label, value}) => (
     <MediaText
         width='25rem'
         left={<label>{label}</label>}
-        right={<input type='text' value={value} />} />
+        right={<input
+                type='text' 
+                value={value}
+                className={classes.UserInput__input} />} />
 );
 
 UserInput.propTypes = {
