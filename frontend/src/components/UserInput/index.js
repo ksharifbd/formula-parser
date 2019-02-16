dd2e3707a9c26d591192af2b5e5e063b1efeb203
ksharifbd <ksharifbd@gmail.com>
2019-02-16 11:09:24 +0600

    Updates UserInput with correct props

--- frontend/src/components/UserInput/index.js
+++ frontend/src/components/UserInput/index.js
@@ -4,19 +4,21 @@ import PropTypes from 'prop-types';
 import MediaText from '../MediaText';
 import classes from './UserInput.module.css';
 
-const UserInput = ({label, value}) => (
+const UserInput = ({label, value, onChange}) => (
     <MediaText
-        width='25rem'
+        marginRight='6rem'
         left={<label>{label}</label>}
         right={<input
                 type='text' 
                 value={value}
-                className={classes.UserInput__input} />} />
+                className={classes.UserInput__input}
+                onChange={onChange} />} />
 );
 
 UserInput.propTypes = {
     label: PropTypes.string.isRequired,
     value: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
 }
 
 export default UserInput;
