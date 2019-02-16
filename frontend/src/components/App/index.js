286f170d9debb677cc82f63946dc9a5f9438fdb9
ksharifbd <ksharifbd@gmail.com>
2019-02-16 15:29:09 +0600

    feteches formulas from api

--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import CalculatorUI from '../CalculatorUI';
+import {getVariables} from '../../utils';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            formulas: [],
+        };
+    }
+
+    componentDidMount() {
+        axios
+            .get('http://localhost:3001/formulas')
+            .then(response => {
+                this.setState({formulas: response.data.map(datum => datum.formula)})
+            })
+            .catch(err => console.log(err));
+    }
+
     render() {
+        const {formulas} = this.state;
         return (
             <form>
                 <ol>
-                    <CalculatorUI
-                        sequence={1}
-                        formula='a+b+c'
-                        variables={['a', 'b', 'c']}
-                        value='2'
-                        onChange={() => {}}
-                        result='11' />
+                    {formulas.length > 0 && formulas.map((formula, index) => (
+                        <CalculatorUI
+                            key={formula}
+                            sequence={index + 1}
+                            formula={formula}
+                            variables={getVariables(formula)}
+                            value='2'
+                            onChange={() => {}}
+                            result={11} />
+                    ))}
                 </ol>
                 <button type='submit'>Submit</button>
             </form>
