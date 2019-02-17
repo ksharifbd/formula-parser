f23467a23ee8ece49e921a31cf406916e1189d46
ksharifbd <ksharifbd@gmail.com>
2019-02-17 18:49:08 +0600

    sets state in local storage before app rendering

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {STORAGE_KEY} from './config.json'
 import './index.css';
 import App from './components/App';
+import getInitialState from './getInitialState';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+getInitialState().then(state => {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(state));
+    ReactDOM.render(<App />, document.getElementById('root'));
+});
