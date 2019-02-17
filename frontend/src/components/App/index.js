d809a11b13c11fe9bc1c5b1519c64d1facba0bd0
ksharifbd <ksharifbd@gmail.com>
2019-02-17 18:52:18 +0600

    constructs output

--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -1,42 +1,85 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 
+import {STORAGE_KEY} from '../../config.json';
 import CalculatorUI from '../CalculatorUI';
-import {getVariables} from '../../utils';
+import {getVariables, fetchFormulas} from '../../utils';
+import UserInput from '../UserInput';
+import getVariablesBySequence from './getVariablesBySequence';
+import calculateByFormula from './calculateByFormula';
 
 class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {
-            formulas: [],
-        };
+        this.state =  JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(evt) {
+        evt.preventDefault();
+
+        const {name, value} = evt.target;
+        
+        this.setState({[name]: value});
+    }
+
+    handleSubmit(evt) {
+        evt.preventDefault();
+
+        const {state} = this;
+
+        const variablesBySequence = getVariablesBySequence(state);
+
+        const output = state.formulas.map((formula, idx) => {
+            const variablesAndValues = variablesBySequence[idx][`${idx + 1}`];
+
+            const values = variablesAndValues.values;
+
+            const result = calculateByFormula(formula, values);
+
+            this.setState({
+                [`result_fid_${idx + 1}`]: result
+            })
+
+
+            return {
+                formula,
+                ...variablesAndValues,
+                result,
+            }
+        });
+
     }
 
     componentDidMount() {
-        axios
-            .get('http://localhost:3001/formulas')
-            .then(response => {
-                this.setState({formulas: response.data.map(datum => datum.formula)})
-            })
-            .catch(err => console.log(err));
+        fetchFormulas().then(formulas => this.setState({formulas}));
     }
 
     render() {
         const {formulas} = this.state;
+        console.log('state', this.state);
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <ol>
-                    {formulas.length > 0 && formulas.map((formula, index) => (
-                        <CalculatorUI
+                    {formulas.length > 0 && formulas.map((formula, idx) => {
+                        const sequence = idx + 1;
+                        return (<CalculatorUI
                             key={formula}
-                            sequence={index + 1}
+                            sequence={sequence}
                             formula={formula}
-                            variables={getVariables(formula)}
-                            value='2'
-                            onChange={() => {}}
-                            result={11} />
-                    ))}
+                            result={this.state[`result_fid_${sequence}`]}>
+                            {getVariables(formula).map(variable => (
+                                <UserInput
+                                    key={`fid_${sequence}_${variable}`}
+                                    name={`fid_${sequence}_${variable}`}
+                                    label={`${variable}:`}
+                                    value={this.state[`fid_${sequence}_${variable}`]}
+                                    onChange={this.handleChange} />
+                            ))}
+                            </CalculatorUI>
+                    )})}
                 </ol>
                 <button type='submit'>Submit</button>
             </form>
