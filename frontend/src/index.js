605ca8069ea76f1b0fc5a89076705c4fc44529cd
ksharifbd <ksharifbd@gmail.com>
2019-02-17 19:07:48 +0600

    rearranges import

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {STORAGE_KEY} from './config.json'
-import './index.css';
 import App from './components/App';
 import getInitialState from './getInitialState';
+import './index.css';
 
 getInitialState().then(state => {
     localStorage.setItem(STORAGE_KEY, JSON.stringify(state));
