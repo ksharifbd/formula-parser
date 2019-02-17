81bb0ae4ba90ef16dee6db7d6c8fbdddabf808db
ksharifbd <ksharifbd@gmail.com>
2019-02-17 22:37:10 +0600

    throws exception in input mismatch

--- frontend/src/components/App/calculateByFormula.js
+++ frontend/src/components/App/calculateByFormula.js
@@ -1,8 +1,12 @@
-function calculateByFormula(formula, obj) {
+function calculateByFormula(formula = '', obj = {}) {
     const evalableFormula = formula
     .split('')
     .map(el => (obj[el] || obj[el] === 0) ? obj[el] : el)
     .join('');
+
+    if(evalableFormula === formula) {
+        throw new ReferenceError('Object keys MUST match formula variables');
+    } 
 
     // eslint-disable-next-line no-eval
     return eval(evalableFormula) || 0;
