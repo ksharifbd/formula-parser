bd8265adcf46290fba48a14cd7202c9293f2b6d8
ksharifbd <ksharifbd@gmail.com>
2019-02-17 23:48:26 +0600

    adds tests for construct initial state

---
+++ frontend/src/utils/constructInitialState/__tests__/index.spec.js
@@ -0,0 +1,25 @@
+import {FORMULA_TRACKING_ID} from '../../../config.json';
+import constructIntialState from '../';
+
+describe('Construct Initial State', () => {
+    const input = ['2*x+y-z', 'a/d*f+5'];
+    const output = {
+        [`${FORMULA_TRACKING_ID}_1_x`]: '',
+        [`${FORMULA_TRACKING_ID}_1_y`]: '',
+        [`${FORMULA_TRACKING_ID}_1_z`]: '',
+        [`${FORMULA_TRACKING_ID}_2_a`]: '',
+        [`${FORMULA_TRACKING_ID}_2_d`]: '',
+        [`${FORMULA_TRACKING_ID}_2_f`]: '',
+        formulas: [],
+        [`result_${FORMULA_TRACKING_ID}_1`]: 0,
+        [`result_${FORMULA_TRACKING_ID}_2`]: 0,
+    };
+
+    it('should return formulas if no input is provided', () => {
+        expect(constructIntialState()).toMatchObject({formulas: []});
+    });
+
+    it('should return correct output when appropriate input is provided', () => {
+        expect(constructIntialState(input)).toMatchObject(output);
+    });
+});
