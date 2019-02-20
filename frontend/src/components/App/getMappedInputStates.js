8193361e9663102d56b8ad33037ff2e2ce9fc9de
ksharifbd <ksharifbd@gmail.com>
2019-02-20 12:18:37 +0600

    filters out results

--- frontend/src/components/App/getMappedInputStates.js
+++ frontend/src/components/App/getMappedInputStates.js
@@ -4,6 +4,7 @@ function getMappedInputStates(state = {}) {
     return Object
     .entries(state)
     .filter(state => state[0].indexOf(`${FORMULA_TRACKING_ID}`) > -1)
+    .filter(state => state[0].indexOf('result')  === -1)
     .map(state => {
         const stateKey = state[0].slice(state[0].indexOf('_') + 1);
         const variable = stateKey.slice(stateKey.indexOf('_' + 1));
